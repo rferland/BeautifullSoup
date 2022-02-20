c81e9737be02d9af3338a265a3250a4767f8f0c2
Raymond Ferland <rferland@diq.ca>
2022-02-20 11:19:55 -0500

    En cours de dev

--- lib/PlanteParser.py
+++ lib/PlanteParser.py
@@ -4,14 +4,19 @@ class PlanteParser():
     
     containerClass=''
     soup = None
+    listeType='li'
+    listeClass='grid__item'
+    nomClass= 'h4 grid-view-item__title product-card__title'
+    prixClass ='price-item price-item--regular'
+    prixSpecialClass = ''
 
     def ParseShopify(self, pPageCountMax=0):
         plantesList = []    
         
-        for item in self.soup.find_all('li', attrs={'class': 'grid__item'}):
+        for item in self.soup.find_all(self.listeType, attrs={f'class': {self.listeClass}}):
 
             plante= Plante.Plante()
-            nomComplet = item.find(class_='h4 grid-view-item__title product-card__title').text
+            nomComplet = item.find(class_= self.nomClass).getText()
             
             nomDetail = nomComplet.split()
             plante.graines = (nomDetail[len(nomDetail)-1]=='Seeds')
@@ -22,15 +27,34 @@ class PlanteParser():
             if len(nomPrix)>1:
                 plante.taille = nomPrix[1].strip()
                 
-            plante.prix=item.find(class_='price-item price-item--regular').text.strip()
-            plante.prixSpecial=item.find(class_='price-item price-item--sale').text.strip()
+            prix= plante.prix=item.find(class_=self.prixClass)
+            if(prix):
+                plante.prix = self.parseRegularPrice(prix.getText())
+                plante.prixSpecial=self.parseSpecialPrice(prix.getText())
+
+            if(self.prixSpecialClass != '' and item.find(class_=self.prixSpecialClass) ):
+                plante.prixSpecial=item.find(class_=self.prixSpecialClass).getText().strip()
                             
             plantesList.append(plante)
 
         return plantesList
 
+    def parseRegularPrice(self, pPrixTexte):
+        Texte = pPrixTexte.replace("$","").replace("\n"," ").split()
+        if("Regular" in Texte):
+            return Texte[2]
+        if len(Texte)>0:
+            return Texte[0]
+        return None
+
+    def parseSpecialPrice(self, pPrixTexte):
+        Texte = pPrixTexte.replace("$","").replace("\n"," ").split()
+        if("Sale" in Texte):
+            return Texte[5]
+        return None
+
     def __init__(self, pSoup, pClass):
-        self.plantesHTML = pSoup.select(f'.{pClass}')
+        self.plantesHTML = pSoup.select(f".{pClass}")
         self.soup=pSoup
         
         
