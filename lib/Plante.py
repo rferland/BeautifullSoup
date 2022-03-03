5439fcc17f546964398c2dcc5bb44b5d57274b36
Raymond Ferland <rferland@diq.ca>
2022-03-03 06:33:30 -0500

    Fonctionnel

--- lib/Plante.py
+++ lib/Plante.py
@@ -1,12 +1,14 @@
 class Plante():
+    site=''
     nom=''
+    nomcomplet = ''
+    date=''
     code =''
-    graines = False
-    taille=''
+    detail=''
     prix=''
     prixSpecial=''
     photos= []
     url = ''
     def __str__(self):
         attrs = vars(self)
-        return '{' +', '.join("%s: %s" % item for item in attrs.items())+'}'
+        return '[{' +', '.join("%s: %s" % item for item in attrs.items())+'}]'
--- lib/PlanteParser.py
+++ lib/PlanteParser.py
@@ -1,31 +1,32 @@
+from datetime import datetime
 from lib import Plante
 
 class PlanteParser():
     
     containerClass=''
     soup = None
-    listeType='li'
-    listeClass='grid__item'
-    nomClass= 'h4 grid-view-item__title product-card__title'
-    prixClass ='price-item price-item--regular'
+    listeType=''
+    listeClass=''
+    nomClass= ''
+    prixClass =''
     prixSpecialClass = ''
+    url=''
 
-    def ParseShopify(self, pPageCountMax=0):
+    def ParseShopify(self, siteName = '', pPageCountMax=0):
         plantesList = []    
         
         for item in self.soup.find_all(self.listeType, attrs={f'class': {self.listeClass}}):
 
             plante= Plante.Plante()
-            nomComplet = item.find(class_= self.nomClass).getText()
-            
-            nomDetail = nomComplet.split()
-            plante.graines = (nomDetail[len(nomDetail)-1]=='Seeds')
+            plante.url=self.url
+            plante.date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            self.nomComplet = item.find(class_= self.nomClass).getText()            
 
-            nomPrix =nomComplet.split('-')
+            nomPrix =self.nomComplet.split('-')
             plante.nom = nomPrix[0].strip()
             plante.code=''.join(filter(str.isalpha, nomPrix[0].upper()))                
             if len(nomPrix)>1:
-                plante.taille = nomPrix[1].strip()
+                plante.detail = nomPrix[1].strip()
                 
             prix= plante.prix=item.find(class_=self.prixClass)
             if(prix):
@@ -35,12 +36,15 @@ class PlanteParser():
             if(self.prixSpecialClass != '' and item.find(class_=self.prixSpecialClass) ):
                 plante.prixSpecial=item.find(class_=self.prixSpecialClass).getText().strip()
                             
-            plantesList.append(plante)
+            plantesList.append(plante.__dict__)
+        
+        print()
+        print(f"{self.url} Total: {len(plantesList)}")
 
         return plantesList
 
     def parseRegularPrice(self, pPrixTexte):
-        Texte = pPrixTexte.replace("$","").replace("\n"," ").split()
+        Texte = pPrixTexte.replace("$","").replace("CAD","").replace("\n"," ").split()
         if("Regular" in Texte):
             return Texte[2]
         if len(Texte)>0:
@@ -53,7 +57,8 @@ class PlanteParser():
             return Texte[5]
         return None
 
-    def __init__(self, pSoup, pClass):
+    def __init__(self, pSoup, pClass, pUrl):
+        self.url=pUrl
         self.plantesHTML = pSoup.select(f".{pClass}")
         self.soup=pSoup
         
